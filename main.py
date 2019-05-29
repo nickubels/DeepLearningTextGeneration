e41b45139d6995443511efbe8c59fe03d8182660
Nick Ubels <n.s.ubels@student.rug.nl>
2019-05-29 13:48:36 +0200

    Setup for file

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+class TextGeneration:
+    def __init__(self):
+        # Here we init stuff
+        print("Init")
+
+    def run(self):
+        print("RENNEN MIENJONG!")
+
+
+def main():
+    generation = TextGeneration()
+    generation.run()
+
+
+if __name__ == '__main__':
+    main()

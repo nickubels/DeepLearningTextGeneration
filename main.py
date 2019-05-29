cac68533b3bad6b7c820941373ecd5dad999cbbf
Nick Ubels <n.s.ubels@student.rug.nl>
2019-05-29 13:56:48 +0200

    Added argparse
    Closes #2

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+import argparse
 
+def get_args():
+    parser = argparse.ArgumentParser(description='Script to generate Trump tweets')
+    parser.add_argument('--data_path', '-d', metavar='STRING', default='./data',
+                        help="Where the CSV with the tweets is stored")
+    parser.add_argument('--output_path', '-o', metavar='STRING', default='./output',
+                        help="Where the output will be stored")
+
+    return parser.parse_args()
 
 class TextGeneration:
     def __init__(self):
         # Here we init stuff
+        self.args = get_args()
         print("Init")
 
     def run(self):
